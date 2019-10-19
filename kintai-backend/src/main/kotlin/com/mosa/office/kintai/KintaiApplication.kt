04502c1f91878d7072ef275710e2b45a076f9b48
hagaitski <i.haga629@gmail.com>
2019-10-19 17:19:20 +0900

    kotlinを入れてみた

---
+++ kintai-backend/src/main/kotlin/com/mosa/office/kintai/KintaiApplication.kt
@@ -0,0 +1,11 @@
+package com.mosa.office.kintai
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KintaiApplication
+
+fun main(args: Array<String>) {
+	runApplication<KintaiApplication>(*args)
+}
