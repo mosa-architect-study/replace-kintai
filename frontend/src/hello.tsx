ac03c4fec9bd78f2e52e635ddbdc97e4e42a6ef9
hagaitski <i.haga629@gmail.com>
2019-10-19 18:45:54 +0900

    lint修正

--- frontend/src/hello.tsx
+++ frontend/src/hello.tsx
@@ -1,8 +1,8 @@
 import * as React from "react";
-import { css } from '@emotion/core';
+import { css } from "@emotion/core";
 
 const style = css({
-  color: 'blue'
+  color: "blue"
 });
 
 export const Hello: React.FC = () => <p css={style}>Hello React</p>;
