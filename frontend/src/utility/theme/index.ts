8b2bc9e80a00a8ecedb10e3fc2e701a5ccc43266
hagaitski <i.haga629@gmail.com>
2019-10-22 22:42:18 +0900

    ファイル名の変更 型の修正

---
+++ frontend/src/utility/theme/index.ts
@@ -0,0 +1,48 @@
+const theme = {
+  palette: {
+    lightblue: "#5FA6F2",
+    baseblue: "#4676AB",
+    darkblue: "#3C5E96",
+    white: "#F2F8FB",
+    black: "#868181",
+    red: "#DD4444"
+  },
+  pcFonts: {
+    base: "14px",
+    xs: "18px",
+    s: "20px",
+    m: "24px",
+    l: "28px",
+    xl: "32px",
+    xxl: "36px",
+    xxxl: "48px"
+  },
+  spFonts: {
+    base: "7px",
+    s: "10px",
+    m: "12px",
+    l: "14px",
+    xl: "16px",
+    xxl: "18px",
+    xxxl: "24px"
+  },
+  opacity: {
+    nomal: 1,
+    half: 0.5,
+    low: 0.2
+  },
+  space: {
+    nothing: "0px",
+    xxxs: "2px",
+    xxs: "4px",
+    xs: "8px",
+    s: "16px",
+    m: "24px",
+    l: "32px",
+    xl: "48px",
+    xxl: "64px",
+    xxxl: "80px"
+  }
+};
+
+export default theme;
