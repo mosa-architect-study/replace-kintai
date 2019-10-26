155f7e09ab63c1f3c23cd243978925a8ddb6d2ac
hagaitski <i.haga629@gmail.com>
2019-10-26 20:20:10 +0900

    themaファイルを適用

--- frontend/src/components/atoms/caption/index.stories.tsx
+++ frontend/src/components/atoms/caption/index.stories.tsx
@@ -1,25 +1,12 @@
 import { Caption } from ".";
 import React from "react";
-import { action } from "@storybook/addon-actions";
 
 export default {
   title: "Caption"
 };
 
-export const size1 = (): JSX.Element => (
-  <Caption size="1" onClick={action("clicked")}>
-    Caption 1
-  </Caption>
-);
+export const size1 = (): JSX.Element => <Caption size="1">Caption 1</Caption>;
 
-export const size2 = (): JSX.Element => (
-  <Caption size="2" onClick={action("clicked")}>
-    Caption 2
-  </Caption>
-);
+export const size2 = (): JSX.Element => <Caption size="2">Caption 2</Caption>;
 
-export const size3 = (): JSX.Element => (
-  <Caption size="3" onClick={action("clicked")}>
-    Caption 3
-  </Caption>
-);
+export const size3 = (): JSX.Element => <Caption size="3">Caption 3</Caption>;
--- frontend/src/components/atoms/caption/index.tsx
+++ frontend/src/components/atoms/caption/index.tsx
@@ -1,17 +1,42 @@
+import * as React from "react";
 import styled from "@emotion/styled";
+import theme from "@/common/theme";
 
 export type CaptionSize = "1" | "2" | "3";
 export interface CaptionProps {
   size: CaptionSize;
 }
 
-const sizeDict: { [P in CaptionSize]: number } = {
-  1: 20,
-  2: 28,
-  3: 36
+const pcSizeDict: { [P in CaptionSize]: string } = {
+  1: theme.pcFonts.s,
+  2: theme.pcFonts.m,
+  3: theme.pcFonts.l
 };
 
-export const Caption = styled.span<CaptionProps>`
-  font-size: ${({ size }): number => sizeDict[size]}px;
-  color: #868181;
+const spSizeDict: { [P in CaptionSize]: string } = {
+  1: theme.spFonts.s,
+  2: theme.spFonts.m,
+  3: theme.spFonts.l
+};
+
+const tagnameDict: { [P in CaptionSize]: "h1" | "h2" | "h3" } = {
+  1: "h3",
+  2: "h2",
+  3: "h1"
+};
+
+type InnerProps = React.PropsWithChildren<{
+  className?: string;
+  size: CaptionSize;
+}>;
+
+const CaptionInner = (props: InnerProps): JSX.Element =>
+  React.createElement(tagnameDict[props.size], props);
+
+export const Caption = styled(CaptionInner)<CaptionProps>`
+  color: ${theme.palette.black};
+  font-size: ${({ size }): string => pcSizeDict[size]};
+  @media (max-width: 480px) {
+    font-size: ${({ size }): string => spSizeDict[size]};
+  }
 `;
