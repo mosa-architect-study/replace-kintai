91a30ed8088df45587c2496a0414f1e842f2ae94
hagaitski <i.haga629@gmail.com>
2019-10-26 18:30:40 +0900

    storybookのwebpackにaliasを追加

--- frontend/.storybook/webpack.config.js
+++ frontend/.storybook/webpack.config.js
@@ -3,5 +3,6 @@ const mainConfig = require("../webpack.config");
 module.exports = ({ config }) => {
     config.module.rules.push(...mainConfig.module.rules)
     config.resolve.extensions = mainConfig.resolve.extensions;
+    config.resolve.alias = mainConfig.resolve.alias;
     return config
 }
