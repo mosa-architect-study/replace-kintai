1434be16858322484f6cb919a98e027adc1bbb28
haga-itsuki <haga-itsuki@dmm.com>
2019-10-29 14:27:20 +0900

    add: stroybook-ress

--- frontend/.storybook/config.js
+++ frontend/.storybook/config.js
@@ -1,4 +1,5 @@
 import { configure } from '@storybook/react';
+import "ress";
 
 // automatically import all files ending in *.stories.js
 configure(require.context('../src/components', true, /stories\.tsx$/), module);
--- frontend/.storybook/webpack.config.js
+++ frontend/.storybook/webpack.config.js
@@ -1,4 +1,9 @@
 const merge = require("webpack-merge")
 const base = require("../webpack.config.base")
 
-module.exports = ({ config }) => merge(config,base);
+module.exports = ({ config }) => {
+    const merged =  merge.smartStrategy({
+        'module.rules': "replace"
+    })(config,base);
+    return merged;
+}
