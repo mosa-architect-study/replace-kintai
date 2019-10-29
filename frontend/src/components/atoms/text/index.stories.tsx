7541275c5eb58b8d93a688ee53e70375fefa1d59
大久保悠樹 <y.okubo@mosa-architect.com>
2019-10-29 23:40:40 +0900

    themeの適用

--- frontend/src/components/atoms/text/index.stories.tsx
+++ frontend/src/components/atoms/text/index.stories.tsx
@@ -7,19 +7,13 @@ export default {
 };
 
 export const size1 = (): JSX.Element => (
-  <Text size="1" onClick={action("clicked")}>
-    Text 1
-  </Text>
+  <Text size="1">Text 1</Text>
 );
 
 export const size2 = (): JSX.Element => (
-  <Text size="2" onClick={action("clicked")}>
-    Text 2
-  </Text>
+  <Text size="2">Text 2</Text>
 );
 
 export const size3 = (): JSX.Element => (
-  <Text size="3" onClick={action("clicked")}>
-    Text 3
-  </Text>
+  <Text size="3">Text 3</Text>
 );
--- frontend/src/components/atoms/text/index.tsx
+++ frontend/src/components/atoms/text/index.tsx
@@ -1,17 +1,26 @@
 import styled from "@emotion/styled";
+import theme from "@/common/theme";
 
 export type TextSize = "1" | "2" | "3";
 export interface TextProps {
   size: TextSize;
 }
 
-const sizeDict: { [P in TextSize]: number } = {
-  1: 20,
-  2: 28,
-  3: 36
+const pcSizeDict: { [P in TextSize]: string } = {
+	1: theme.pcFonts.s,
+	2: theme.pcFonts.m,
+	3: theme.pcFonts.l
+};
+const spSizeDict: { [P in TextSize]: string } = {
+	1: theme.spFonts.s,
+	2: theme.spFonts.m,
+	3: theme.spFonts.l
 };
 
-export const Text = styled.span<TextProps>`
-  font-size: ${({ size }): number => sizeDict[size]}px;
-  color: #868181;
+export const Text = styled.p<TextProps>`
+  color: ${theme.palette.black};
+  font-size: ${({ size }): string => pcSizeDict[size]};
+  @media (max-width: 480px) {
+    font-size: ${({ size }): string => spSizeDict[size]};
+  }
 `;
