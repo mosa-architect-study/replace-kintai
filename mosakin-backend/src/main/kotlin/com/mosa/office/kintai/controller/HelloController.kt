2b734c8ce6e3898af12972e5fb19009a5d3389e5
hagaitski <i.haga629@gmail.com>
2019-10-31 22:45:08 +0900

    userを作成

--- mosakin-backend/src/main/kotlin/com/mosa/office/kintai/controller/HelloController.kt
+++ mosakin-backend/src/main/kotlin/com/mosa/office/kintai/controller/HelloController.kt
@@ -1,5 +1,6 @@
 package com.mosa.office.kintai.controller
 
+import com.mosa.office.kintai.model.AdminFlg
 import com.mosa.office.kintai.model.User
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
@@ -9,7 +10,7 @@ class HelloController {
 
     @GetMapping("/hello")
     fun hello(): User {
-        return User("Bob",18);
+        return User("Bob","00000001",AdminFlg.AdminUser);
     }
 
 }
