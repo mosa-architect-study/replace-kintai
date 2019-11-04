263cc829228f011e1ee72ddadaf8dc85eee688d3
yamauchi <mosa@mosanoMacBook-Pro.local>
2019-11-05 01:02:37 +0900

    atoms button

---
+++ frontend/src/components/atoms/button/index.stories.tsx
@@ -0,0 +1,12 @@
+import { Button } from ".";
+import React from "react";
+
+export default {
+  title: "Button"
+};
+
+export const Button1 = (): JSX.Element => (
+  <Button color="1" backgroundColor="1">
+    ボタン
+  </Button>
+);
---
+++ frontend/src/components/atoms/button/index.tsx
@@ -0,0 +1,29 @@
+import styled from "@emotion/styled";
+import * as Constant from "./constant";
+import { paletteDict } from "@/common/theme";
+
+interface ButtonProps {
+  color: Constant.ButtonFontColorType;
+  backgroundColor: Constant.ButtonBackColorType;
+}
+
+const ButtonStyle = styled.button`
+  width: 98px;
+  height: 44px;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  border-radius: 10px;
+  &:hover {
+    box-shadow: none;
+    transform: translate3d(0, 3px, 0);
+    transition-duration: 0.25s;
+  }
+  &:focus {
+    outline: 0;
+  }
+`;
+
+export const Button = styled(ButtonStyle)<ButtonProps>`
+  color: ${({ color }) => paletteDict[Constant.ButtonFontColor[color]]};
+  background-color: ${({ backgroundColor }) =>
+    paletteDict[Constant.ButtonBackColor[backgroundColor]]};
+`;
