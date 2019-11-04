14e39ec3d9d719114ed6055f64a07c67bd447736
hagaitski <i.haga629@gmail.com>
2019-11-04 16:06:15 +0900

    globalcssを追加

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "ress";
+import "./static/global.css";
 import { App } from "./sample/react-router";
 
 ReactDOM.render(<App />, document.getElementById("app"));
