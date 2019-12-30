fd6cc4baaeb1817a129d323b7a1b624350535a13
y.hoshi <y.hoshi@mosa-architect.com>
2019-12-31 02:38:44 +0900

    指摘内容反映

--- mosakin-backend/src/main/kotlin/com/mosa/office/kintai/controller/DeletePaidController.kt
+++ mosakin-backend/src/main/kotlin/com/mosa/office/kintai/controller/DeletePaidController.kt
@@ -2,12 +2,13 @@ package com.mosa.office.kintai.controller
 
 import com.mosa.office.kintai.controller.model.DeletePaidModel
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 class DeletePaidController {
 
-    @GetMapping("/delete")
+    @PostMapping("/delete")
     fun delete() :DeletePaidModel {
         val success: Boolean = true
         val message: String = "君の指定した有給取得日はきちんと削除されたよ★ミ"
--- mosakin-backend/src/main/kotlin/com/mosa/office/kintai/controller/model/DeletePaid.kt
+++ mosakin-backend/src/main/kotlin/com/mosa/office/kintai/controller/model/DeletePaid.kt
@@ -1,3 +1,3 @@
 package com.mosa.office.kintai.controller.model
 
-data class DeletePaidModel(val success : Boolean,val errorMessage : String)
+data class DeletePaidModel(val success : Boolean,val message : String)
