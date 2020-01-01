2a5a6853f54d78350b233617f7fea17758933117
hagaitski <i.haga629@gmail.com>
2020-01-01 11:14:26 +0900

    fix: markup

--- mosakin-frontend/src/components/pages/404.tsx
+++ mosakin-frontend/src/components/pages/404.tsx
@@ -1,11 +1,25 @@
 import mosakin from "../../static/mosakin.png";
 import React from "react";
+import styled from "@emotion/styled";
+
+const MosakinHouse = styled.img`
+  height: 400px;
+`;
+
+const NotFoundTitle = styled.p`
+  font-size: 50px;
+`;
+
+const NotFoundWrapper = styled.main`
+  text-align: center;
+  padding-top: 20px;
+`;
 
 export const NotFoundPage: React.FC = () => {
   return (
-    <section>
-      <p>Not Found</p>
-      <img src={mosakin}></img>;
-    </section>
+    <NotFoundWrapper>
+      <NotFoundTitle>Not Found</NotFoundTitle>
+      <MosakinHouse src={mosakin}></MosakinHouse>;
+    </NotFoundWrapper>
   );
 };
--- mosakin-frontend/src/sample/login.tsx
+++ mosakin-frontend/src/sample/login.tsx
@@ -10,6 +10,10 @@ interface User {
   photoURL: string | null;
 }
 
+const UserInfoWrapper = styled.section`
+  display: flex;
+`;
+
 export const AuthButton = () => {
   const [user, setUser] = React.useState<User | null | "Loading">("Loading");
   React.useEffect(() => {
@@ -30,27 +34,26 @@ export const AuthButton = () => {
     user === "Loading" ? (
       <p>Loading...</p>
     ) : (
-      <section>
-        <div>User Info</div>
+      <UserInfoWrapper>
         <div>
-          <div>
+          <p>
             <b>User: </b>
             {user.displayName}
-          </div>
-          <div>
+          </p>
+          <p>
             <b>E-mail: </b>
             {user.email}
-          </div>
+          </p>
         </div>
         {user.photoURL && (
-          <img style={{ height: "100px" }} src={user.photoURL}></img>
+          <img style={{ height: "44px" }} src={user.photoURL}></img>
         )}
         <div>
           <Button color="1" onClick={_logout} backgroundColor="1">
             Logout
           </Button>
         </div>
-      </section>
+      </UserInfoWrapper>
     )
   ) : (
     <Button color="1" backgroundColor="1" onClick={login}>
