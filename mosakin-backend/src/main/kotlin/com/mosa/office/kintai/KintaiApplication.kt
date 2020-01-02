ebff19f0a248b7e518cd9cc0bba60d28809b3232
hagaitski <i.haga629@gmail.com>
2020-01-03 00:33:25 +0900

    fix: o import

--- mosakin-backend/src/main/kotlin/com/mosa/office/kintai/KintaiApplication.kt
+++ mosakin-backend/src/main/kotlin/com/mosa/office/kintai/KintaiApplication.kt
@@ -1,6 +1,5 @@
 package com.mosa.office.kintai
 
-import org.jetbrains.exposed.sql.Database
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
