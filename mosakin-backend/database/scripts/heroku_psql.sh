8373c6628961e7e7f38e51c2d5053345fd3ca6c0
hagaitski <i.haga629@gmail.com>
2020-01-07 19:28:15 +0900

    fix: move docker image to docker hub

--- mosakin-backend/database/scripts/heroku_psql.sh
+++ mosakin-backend/database/scripts/heroku_psql.sh
@@ -1,19 +1,8 @@
 #!/bin/bash
-
-NAME_IMAGE='heroku-psql'
-
-if [ "$(docker image ls -q ${NAME_IMAGE})" ]; then
-    echo "すでにある ${NAME_IMAGE} イメージを利用するよ。"
-else
-    echo "${NAME_IMAGE} イメージがないからビルドするよ。"
-    echo 'Now building image ...'
-    docker build . -t ${NAME_IMAGE} -f ./heroku_psql.Dockerfile
-fi
-
 touch .netrc
 docker run -it --rm \
     -v $PWD/sql:/sql \
     -v $PWD/.netrc:/root/.netrc \
     -v $PWD/scripts:/scripts \
-    ${NAME_IMAGE} \
+    hagaitski/heroku-psql-cli \
     /bin/bash -c 'sh scripts/heroku_login.sh && /bin/bash'
