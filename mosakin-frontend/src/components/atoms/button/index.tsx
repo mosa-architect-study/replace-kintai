81f6d85b85d512636cf6daae9dc94e1bc1d34e50
k.sugawara <k.sugawara@mosa-architect.com>
2020-01-19 13:43:20 +0900

    fix: button componrnt

--- mosakin-frontend/src/components/atoms/button/index.tsx
+++ mosakin-frontend/src/components/atoms/button/index.tsx
@@ -1,20 +1,39 @@
+import * as React from "react";
 import styled from "@emotion/styled";
 import * as Constant from "./constant";
 import { paletteDict } from "@/common/theme";
 
 interface ButtonProps {
+  type: "button" | "submit" | "reset";
   backgroundColor: Constant.ButtonBackColorType;
   width: Constant.ButtonWidthType;
   height: Constant.ButtonHeightType;
+  onClick: () => void;
+  children: React.ReactNode;
 }
 
-//FIXME: ホバー時の色固定か
-const ButtonStyle = styled.button`
+const StyledButton = styled.button<ButtonProps>`
+  position: relative;
+  width: ${({ width }) => Constant.ButtonWidth[width]};
+  height: ${({ height }) => Constant.ButtonHeight[height]};
   box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
   border-radius: 10px;
+  background-color: ${({ backgroundColor }) =>
+    paletteDict[Constant.ButtonBackColor[backgroundColor]]};
   &:hover {
-    background-color: ${paletteDict.accent};
     transition-duration: 0.5s;
+    &::before {
+      content: "";
+      position: absolute;
+      top: 0;
+      right: 0;
+      bottom: 0;
+      left: 0;
+      width: ${({ width }) => Constant.ButtonWidth[width]};
+      height: ${({ height }) => Constant.ButtonHeight[height]};
+      background: ${paletteDict.whitesmoke};
+      border-radius: 10px;
+    }
   }
   &:active {
     box-shadow: none;
@@ -26,9 +45,21 @@ const ButtonStyle = styled.button`
   }
 `;
 
-export const Button = styled(ButtonStyle)<ButtonProps>`
-  background-color: ${({ backgroundColor }) =>
-    paletteDict[Constant.ButtonBackColor[backgroundColor]]};
-  width: ${({ width }) => Constant.ButtonWidth[width]};
-  height: ${({ height }) => Constant.ButtonHeight[height]};
-`;
+const Button: React.FC<ButtonProps> = (props: ButtonProps) => {
+  const { type, backgroundColor, width, height, onClick, children } = props;
+  return (
+    <div>
+      <StyledButton
+        type={type}
+        backgroundColor={backgroundColor}
+        width={width}
+        height={height}
+        onClick={onClick}
+      >
+        {children}
+      </StyledButton>
+    </div>
+  );
+};
+
+export { Button };
