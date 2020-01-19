18d6cea21ffeab8bbcfe1eeba4c8b5ed695b0994
大久保悠樹 <y.okubo@mosa-architect.com>
2020-01-19 18:50:37 +0900

    新規申請(修正漏れの修正)

--- mosakin-frontend/src/components/molecules/paid-time-type/index.stories.tsx
+++ mosakin-frontend/src/components/molecules/paid-time-type/index.stories.tsx
@@ -2,8 +2,8 @@ import React from "react";
 import { storiesOf } from "@storybook/react";
 import { PaidTimeType } from ".";
 
-storiesOf("molecules/PaidTimeArea", module).add("PaidTimeArea", () => {
-  const [value, setValue] = React.useState("all-day");
+storiesOf("molecules/PaidTimeType", module).add("PaidTimeType", () => {
+  const [value, setValue] = React.useState("ALL-DAY");
   return (
     <div>
       <PaidTimeType value={value} onClick={setValue}></PaidTimeType>
