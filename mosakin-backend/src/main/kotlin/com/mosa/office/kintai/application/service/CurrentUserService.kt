33dca3fe096e52885a4a2cbad2bc0094ce3c5163
hagaitski <i.haga629@gmail.com>
2020-01-26 11:59:08 +0900

    fix: remove unused

--- mosakin-backend/src/main/kotlin/com/mosa/office/kintai/application/service/CurrentUserService.kt
+++ mosakin-backend/src/main/kotlin/com/mosa/office/kintai/application/service/CurrentUserService.kt
@@ -2,7 +2,6 @@ package com.mosa.office.kintai.application.service
 
 import org.springframework.stereotype.Component
 import org.springframework.web.context.annotation.RequestScope
-import javax.servlet.http.HttpServletRequest
 
 interface CurrentUserService {
     fun getUser():String;
@@ -11,20 +10,6 @@ interface CurrentUserService {
 interface CurrentUserHolder {
     fun setUserId(userId:String);
 }
-
-//@Component
-//class CurrentUserServiceImpl(
-//    private val request: HttpServletRequest,
-//    private val authService: AuthorizedUserIdService
-//) : CurrentUserService {
-//    override fun getUser(): String {
-//        val token = request.getHeader("Authorization");
-//        return when(val result = authService.getUserId(token)){
-//            is Authorized -> result.userId
-//            is UnAuthorized -> throw Exception("認証に失敗しました。");
-//        }
-//    }
-//}
 
 @Component
 @RequestScope
