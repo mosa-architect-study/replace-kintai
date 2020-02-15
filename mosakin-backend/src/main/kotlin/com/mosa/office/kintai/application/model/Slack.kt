a9f0258fd35c7fdf4b3ccb0ec0f555552d5f7b4b
hagaitski <i.haga629@gmail.com>
2020-02-15 13:28:24 +0900

    fix: refactor slack

--- mosakin-backend/src/main/kotlin/com/mosa/office/kintai/application/model/Slack.kt
+++ mosakin-backend/src/main/kotlin/com/mosa/office/kintai/application/model/Slack.kt
@@ -3,10 +3,6 @@ package com.mosa.office.kintai.application.model
 import com.mosa.office.kintai.domain.model.PaidTimeType
 import java.time.LocalDate
 
-data class SlackMessageProperty (
-        var text: String?
-)
-
 data class SlackAddPaidInfo(
         val paidAcquisitionDate : LocalDate,
         val paidTimeType : PaidTimeType,
--- mosakin-backend/src/main/kotlin/com/mosa/office/kintai/application/service/SlackService.kt
+++ mosakin-backend/src/main/kotlin/com/mosa/office/kintai/application/service/SlackService.kt
@@ -3,9 +3,8 @@ package com.mosa.office.kintai.application.service
 import com.mosa.office.kintai.application.model.SlackAddPaidInfo
 import com.mosa.office.kintai.application.model.SlackUpdatePaidInfo
 import com.mosa.office.kintai.application.model.SlackMessageException
-import com.mosa.office.kintai.application.model.SlackMessageProperty
 import com.mosa.office.kintai.config.SlackConfig
-import org.springframework.beans.factory.annotation.Autowired
+import org.slf4j.Logger
 import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType
 import org.springframework.http.RequestEntity
@@ -15,31 +14,42 @@ import java.net.URI
 
 @Service
 class SlackService(
-) {
-    @Autowired
-    lateinit var slackConfig: SlackConfig
+    private val slackConfig: SlackConfig,
+    private val logger: Logger
+)
+{
 
     // TODO 送る文字列の作成
     fun postAddSlackMessage(input: SlackAddPaidInfo) {
-        val text: SlackMessageProperty = SlackMessageProperty(" \n新規： *" + "ユーザー名" + "*\n日時： *" + input.paidAcquisitionDate + " (" +  ")" + "  [" + input.paidTimeType + "]*\n```\n" + input.paidReason + "```");
-        val jsonData:String = slackConfig.getSlackMessageUrl() ?: return throw SlackMessageException()
-        val request: RequestEntity<SlackMessageProperty> = RequestEntity.post(URI(jsonData)).accept(MediaType.APPLICATION_JSON).body(text);
-        val restTemplate = RestTemplate()
-        val res = restTemplate.exchange(request, String::class.java)
-        if (res.statusCode != HttpStatus.OK) {
-            return throw SlackMessageException()
-        }
+        val text = SlackMessageProperty(" \n新規： *" + "ユーザー名" + "*\n日時： *" + input.paidAcquisitionDate + " (" +  ")" + "  [" + input.paidTimeType + "]*\n```\n" + input.paidReason + "```");
+        postMessage(text)
     }
 
     // TODO 送る文字列の作成
     fun postUpdateSlackMessage(input: SlackUpdatePaidInfo) {
-        val text: SlackMessageProperty = SlackMessageProperty(" \n更新： *" + "ユーザー名" + "*\n更新前日時： *" + input.beforePaidAcquisitionDate + " (" + ")" + "  [" + input.beforePaidTimeType + "]*\n更新後日時： *" + input.paidAcquisitionDate + " (" + ")" + "  [" + input.paidTimeType + "]*\n```\n" + input.paidReason + "```");
+        val text = SlackMessageProperty(" \n更新： *" + "ユーザー名" + "*\n更新前日時： *" + input.beforePaidAcquisitionDate + " (" + ")" + "  [" + input.beforePaidTimeType + "]*\n更新後日時： *" + input.paidAcquisitionDate + " (" + ")" + "  [" + input.paidTimeType + "]*\n```\n" + input.paidReason + "```");
+        postMessage(text)
+    }
+
+    private fun postMessage(requestBody:SlackMessageProperty) {
         val jsonData:String = slackConfig.getSlackMessageUrl() ?: return throw SlackMessageException()
-        val request: RequestEntity<SlackMessageProperty> = RequestEntity.post(URI(jsonData)).accept(MediaType.APPLICATION_JSON).body(text);
+        val request: RequestEntity<SlackMessageProperty> = RequestEntity.post(URI(jsonData)).accept(MediaType.APPLICATION_JSON).body(requestBody);
         val restTemplate = RestTemplate()
-        val res = restTemplate.exchange(request, String::class.java)
+        val res = try {
+            restTemplate.exchange(request, String::class.java)
+        } catch (e:Exception) {
+            logger.error(e.message)
+            throw SlackMessageException()
+        }
         if (res.statusCode != HttpStatus.OK) {
-            return throw SlackMessageException()
+            logger.error("Sending message to slack failed with status code ${res.statusCode}")
+            throw SlackMessageException()
         }
     }
+
 }
+
+
+private data class SlackMessageProperty (
+    val text: String
+)
