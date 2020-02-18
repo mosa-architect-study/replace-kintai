323a28e3b0fe604977bccf93e975b86cef35541e
yamauchi <mosa@mosanoMacBook-Pro.local>
2020-02-18 21:09:19 +0900

    pc-navigation-barでも使えるように

--- mosakin-frontend/src/components/molecules/SpHeader/index.stories.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { storiesOf } from "@storybook/react";
-import { SpHeader } from "./index";
-
-storiesOf("molecules/Header", module).add("SpHeader", () => (
-  <>
-    <SpHeader />
-  </>
-));
---
+++ mosakin-frontend/src/components/molecules/header/index.tsx
@@ -0,0 +1,20 @@
+import styled from "@emotion/styled";
+import { paletteDict } from "@/common/theme";
+
+export const HeaderWrapper = styled.div`
+  display: flex;
+  position: fixed;
+  width: 100%;
+  height: 70px;
+  padding-left: 2.5%;
+  padding-right: 4%;
+  background-color: ${paletteDict.base};
+  border-bottom: 1px solid ${paletteDict.white};
+  justify-content: space-between;
+  align-items: center;
+  line-height: 0;
+  @media (max-width: 480px) {
+    height: 80px;
+    padding: 0 5%;
+  }
+`;
