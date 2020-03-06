f3c848a855b16975594d88632d16f9f1f58a851e
t.omi <t.omi@mosa-architect.com>
2020-03-06 19:49:15 +0900

    カラム名変更、外部キー作成

--- mosakin-backend/database/sql/xx_ddl.sql
+++ mosakin-backend/database/sql/xx_ddl.sql
@@ -1,34 +1,37 @@
-CREATE TABLE user_info (
-    user_id VARCHAR(255),
-    user_name VARCHAR(255),
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    role_id INTEGER,
-    PRIMARY KEY(user_id,role_id)
-);
-
 CREATE TABLE role_info (
     role_id INTEGER,
     role_name VARCHAR(255),
     PRIMARY KEY(role_id)
 );
 
-CREATE TABLE annual_paid_number (
-    paid_id VARCHAR(255),
+CREATE TABLE user_info (
     user_id VARCHAR(255),
+    user_name VARCHAR(255),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    role_id INTEGER NOT NULL,
+    PRIMARY KEY(user_id),
+    FOREIGN KEY (role_id) REFERENCES role_info(role_id)
+);
+
+CREATE TABLE annual_paid_number (
+    annual_paid_number_id VARCHAR(255),
+    user_id VARCHAR(255) NOT NULL,
     fiscal_year INTEGER,
     paid_acquisition_date DOUBLE PRECISION,
     carry_forward DOUBLE PRECISION,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    PRIMARY KEY(paid_id,user_id)
+    PRIMARY KEY(annual_paid_number_id),
+    FOREIGN KEY (user_id) REFERENCES user_info(user_id)
 );
 
 CREATE TABLE paid_info (
-    paid_act_id VARCHAR(255),
-    user_id VARCHAR(255),
+    paid_id VARCHAR(255),
+    user_id VARCHAR(255) NOT NULL,
     paid_acquisition_date DATE,
     paid_time_type VARCHAR(8),
     paid_reason VARCHAR(255),
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    PRIMARY KEY(paid_act_id,user_id)
+    PRIMARY KEY(paid_id),
+    FOREIGN KEY (user_id) REFERENCES user_info(user_id)
 );
 
--- mosakin-backend/database/sql/xx_delete.sql
+++ mosakin-backend/database/sql/xx_delete.sql
@@ -1,4 +1,4 @@
-delete from user_info;
-delete from role_info;
 delete from annual_paid_number;
 delete from paid_info;
+delete from user_info;
+delete from role_info;
