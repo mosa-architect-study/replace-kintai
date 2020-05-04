aff2d63da40ae5eba38eddc7224fc27f39f6a1bd
hagaitski <i.haga629@gmail.com>
2020-05-05 02:02:50 +0900

    fix: sample data

--- mosakin-backend/database/sql/02_sampledata.sql
+++ mosakin-backend/database/sql/02_sampledata.sql
@@ -1,5 +1,7 @@
 INSERT INTO role_info(role_id,role_name) VALUES (0,'COMMON');
 INSERT INTO role_info(role_id,role_name) VALUES (1,'ADMIN');
-INSERT INTO user_info(user_id,user_name,role_id) VALUES ('mosaarchitect.study@gmail.com','猛者オジ',0);
-INSERT INTO annual_paid_number(user_id,fiscal_year,paid_acquisition_date,carry_forward) VALUES ('mosaarchitect.study@gmail.com',2019,15,0);
-INSERT INTO paid_info(paid_id,user_id,paid_acquisition_date,paid_time_type,paid_reason) VALUES ('jaoi1q1r','mosaarchitect.study@gmail.com','2019/12/01','ALL_DAY','ここはお休みをする理由を書くﾖ‼️オジｻﾝ、今日は頭が痛くて😨お休みするね💦🛌ﾅﾝﾁｬｯﾃ(^o^)😘');
+INSERT INTO user_info(user_id,user_name,role_id) VALUES ('mosaarchitect.study@gmail.com','もさはなこ',1);
+INSERT INTO user_info(user_id,user_name,role_id) VALUES ('00000001','猛者オジ',0);
+INSERT INTO annual_paid_number(user_id,fiscal_year,paid_acquisition_date,carry_forward) VALUES ('00000001',2020,15,0);
+INSERT INTO annual_paid_number(user_id,fiscal_year,paid_acquisition_date,carry_forward) VALUES ('mosaarchitect.study@gmail.com',2020,13,0);
+INSERT INTO paid_info(paid_id,user_id,paid_acquisition_date,paid_time_type,paid_reason) VALUES ('jaoi1q1r','00000001','2019-12-01','ALL_DAY','ここはお休みをする理由を書くﾖ‼️オジｻﾝ、今日は頭が痛くて😨お休みするね💦🛌ﾅﾝﾁｬｯﾃ(^o^)😘');
