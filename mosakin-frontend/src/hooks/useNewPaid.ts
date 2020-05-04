5d8e899d6a7a3788056f4166aff20cd272b01b21
hagaitski <i.haga629@gmail.com>
2020-05-04 21:37:49 +0900

    add: user info

--- mosakin-frontend/src/hooks/useNewPaid.ts
+++ mosakin-frontend/src/hooks/useNewPaid.ts
@@ -2,23 +2,24 @@ import { useState } from "react";
 import { axios } from "@/common/api/axios";
 import { NewPaidViewModel, NewPaidItem } from "@/models/models/newPaid";
 import { convertStatusIntoMessage } from "@/hooks/useErrorInfo";
+import { useLoginInfo } from "@/context/LoginContext";
 
 export const useNewPaid = (): NewPaidViewModel => {
   const [dateValue, dateSetValue] = useState("");
   const [paidTimeValue, paidTimeOnChange] = useState("");
   const [reasonValue, reasonSetValue] = useState("");
   const [errorMessage, setErrorMessage] = useState("");
+  const user = useLoginInfo();
 
-  // FIXME: 管理者情報を取得して表示する処理を追記してください
   const createData: NewPaidItem = {
-    userName: "芳賀樹生",
+    userName: user.user.name,
     dateValue: dateValue,
     dateOnChange: dateSetValue,
     paidTimeValue: paidTimeValue,
     paidTimeOnChange: paidTimeOnChange,
     reasonValue: reasonValue,
     reasonOnChange: reasonSetValue,
-    adminFlg: true
+    adminFlg: user.user.role === "ADMIN"
   };
   const onSubmit = () => {
     axios
