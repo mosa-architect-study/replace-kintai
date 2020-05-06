68bffa6687938a7bd6d1e9dc02916f6d8fca7384
hagaitski <i.haga629@gmail.com>
2020-05-06 17:29:19 +0900

    add: admin list api

--- mosakin-backend/src/main/kotlin/com/mosa/office/kintai/application/usecase/GetPaidListUseCase.kt
+++ mosakin-backend/src/main/kotlin/com/mosa/office/kintai/application/usecase/GetPaidListUseCase.kt
@@ -2,10 +2,22 @@ package com.mosa.office.kintai.application.usecase
 
 import com.mosa.office.kintai.application.service.CurrentUserService
 import com.mosa.office.kintai.application.transaction.TransactionBoundary
+import com.mosa.office.kintai.config.AuthenticationException
 import com.mosa.office.kintai.domain.model.*
 import org.springframework.stereotype.Component
 import java.time.LocalDate
-import java.util.*
+
+
+fun calcPaidSummaryHeader(paidList:List<Paid>,userAnnualPaid: UserAnnualPaid):PaidListSummaryHeader{
+    val currentPaidAcquisitionNumber = PaidNumber.fromTimeTypes(paidList.map { it.paidTimeType })
+    return PaidListSummaryHeader(
+        userAnnualPaid.carryForward,
+        userAnnualPaid.annualPaidNumber,
+        userAnnualPaid.annualPaidNumber minus currentPaidAcquisitionNumber,
+        currentPaidAcquisitionNumber
+    )
+}
+
 
 @Component
 class GetPaidListUseCase(
@@ -23,25 +35,52 @@ class GetPaidListUseCase(
                     currentUserService.getUser(),
                     LocalDate.now().year /* TODO ユーザーが入力した年数で表示 */
                 ) ?: UserAnnualPaid(PaidNumber(0.0),PaidNumber(0.0)) //TODO とりあえず0を埋めてるけど、例外でもいい気がする
-            val currentPaidAcquisitionNumber = PaidNumber.fromTimeTypes(list.map { it.paidTimeType })
-            val header = PaidListSummaryHeader(
-                userAnnualPaid.carryForward,
-                userAnnualPaid.annualPaidNumber,
-                userAnnualPaid.annualPaidNumber minus currentPaidAcquisitionNumber,
-                currentPaidAcquisitionNumber
+            PaidListSummary(
+                calcPaidSummaryHeader(list,userAnnualPaid),
+                list.map { PaidListSummaryListItem(it) }
             )
-            PaidListSummary(header,list.map { PaidListSummaryListItem(it) })
+        }
+    }
+
+
+}
+
+//CQRSパターン
+@Component
+class GetAllUserPaidListUseCase(
+    private val query: AllUserPaidListSummaryListQuery,
+    private val transaction : TransactionBoundary,
+    private val currentUserService: CurrentUserService,
+    private val userRepository: UserRepository,
+    private val paidRepository: PaidRepository
+){
+    fun getAllUserPaidSummary() : List<AllUserPaidListSummaryItem> {
+        return transaction.start {
+            // FIXME 管理者系の処理は共通化したい
+            val userId = currentUserService.getUser()
+            val user = userRepository.getUser(userId)
+            if(user?.adminFlag != AdminFlg.ADMIN) throw AuthenticationException()
+
+            val paidMap = paidRepository
+                .getAll()
+                .groupBy { it.paidAcquisitionUserId }
+
+            query.get(LocalDate.now().year/* TODO ユーザーが入力した年数で表示 */)
+                .map {
+                AllUserPaidListSummaryItem(
+                    it.user,
+                    calcPaidSummaryHeader(
+                        paidMap[it.user.userId] ?: emptyList(),
+                        it.annualPaid
+                    )
+                )
+            }
         }
     }
 }
 
-data class PaidListSummaryHeader(
-    val carryForward : PaidNumber,
-    val annualPaidNumber : PaidNumber,
-    val leftPaidNumber: PaidNumber,
-    val currentPaidAcquisitionNumber: PaidNumber
-)
 
+// 有給1つ
 class PaidListSummaryListItem(
     paid : Paid
 ) {
@@ -50,7 +89,37 @@ class PaidListSummaryListItem(
     val paidTimeType = paid.paidTimeType
 }
 
+// 集計結果
+data class PaidListSummaryHeader(
+    val carryForward : PaidNumber,
+    val annualPaidNumber : PaidNumber,
+    val leftPaidNumber: PaidNumber,
+    val currentPaidAcquisitionNumber: PaidNumber
+)
+
+
+// 有給一覧および集計結果
 class PaidListSummary(
-    val header:PaidListSummaryHeader,
+    val header: PaidListSummaryHeader,
     val list: List<PaidListSummaryListItem>
 )
+
+// ユーザーごとの有給集計結果
+class AllUserPaidListSummaryItem(
+    val user: User,
+    val header: PaidListSummaryHeader
+)
+
+
+// Query
+
+// ユーザーごとの年次有給数
+class AllUserPaidListSummaryListQueryResultItem(
+    val user: User,
+    val annualPaid: UserAnnualPaid
+)
+
+interface AllUserPaidListSummaryListQuery{
+    fun get(year: Int) : List<AllUserPaidListSummaryListQueryResultItem>
+}
+
--- mosakin-backend/src/main/kotlin/com/mosa/office/kintai/controller/PaidListController.kt
+++ mosakin-backend/src/main/kotlin/com/mosa/office/kintai/controller/PaidListController.kt
@@ -1,20 +1,25 @@
 package com.mosa.office.kintai.controller
 
-import com.mosa.office.kintai.application.usecase.GetPaidListUseCase
-import com.mosa.office.kintai.application.usecase.PaidListSummary
-import com.mosa.office.kintai.application.usecase.PaidListSummaryHeader
-import com.mosa.office.kintai.application.usecase.PaidListSummaryListItem
+import com.mosa.office.kintai.application.usecase.*
 import com.mosa.office.kintai.domain.model.Paid
 import com.mosa.office.kintai.domain.model.PaidNumber
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-class PaidListController(private val usecase: GetPaidListUseCase) {
+class PaidListController(
+    private val getPaidListUseCase: GetPaidListUseCase,
+    private val getAllUserPaidListUseCase: GetAllUserPaidListUseCase
+) {
 
     @GetMapping("/list")
     fun getPaidList() : PaidListSummary {
-        return usecase.getPaidSummary();
+        return getPaidListUseCase.getPaidSummary();
+    }
+
+    @GetMapping("/admin/list")
+    fun getAllPaidList() : List<AllUserPaidListSummaryItem> {
+        return getAllUserPaidListUseCase.getAllUserPaidSummary();
     }
 
 }
