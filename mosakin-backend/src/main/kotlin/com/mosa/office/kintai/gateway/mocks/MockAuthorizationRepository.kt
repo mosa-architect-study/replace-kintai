c0f420d1c7476eda0b037f901eb76fcfb2a98252
haga-itsuki <haga-itsuki@dmm.com>
2020-05-10 22:27:35 +0900

    fix: refactor config

--- mosakin-backend/src/main/kotlin/com/mosa/office/kintai/gateway/mocks/MockAuthorizationRepository.kt
+++ mosakin-backend/src/main/kotlin/com/mosa/office/kintai/gateway/mocks/MockAuthorizationRepository.kt
@@ -1,13 +1,24 @@
 package com.mosa.office.kintai.gateway.mocks
 
+import com.google.firebase.auth.FirebaseAuth
 import com.mosa.office.kintai.application.service.AuthorizationResult
 import com.mosa.office.kintai.application.service.Authorized
 import com.mosa.office.kintai.application.service.AuthorizationRepository
+import org.slf4j.Logger
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean
+import org.springframework.context.annotation.Profile
+import org.springframework.stereotype.Component
 
-class MockAuthorizationRepository : AuthorizationRepository {
+@Component
+@ConditionalOnMissingBean(FirebaseAuth::class)
+@Profile(value = ["dev","default"])
+class MockAuthorizationRepository(
+        logger: Logger
+) : AuthorizationRepository {
+    init {
+        logger.warn("Mock Implementation")
+    }
     override fun getUserId(token:String): AuthorizationResult {
-        println("TOKEN : $token");
-        // TODO ユーザーIDの取得
         return Authorized("00000001")
     }
 }
