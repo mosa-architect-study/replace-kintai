7b9575d73d5d1981542763f8114720b5d09843e0
hagaitski <i.haga629@gmail.com>
2020-06-15 23:59:00 +0900

    fix: lint tp js

--- mosakin-frontend/jest.config.js
+++ mosakin-frontend/jest.config.js
@@ -1,9 +1,9 @@
 module.exports = {
   moduleFileExtensions: ["ts", "tsx", "js"],
   moduleNameMapper: {
-    "^@/(.+)": "<rootDir>/src/$1"
+    "^@/(.+)": "<rootDir>/src/$1",
   },
   testMatch: ["**/*.test.+(ts|tsx|js)"],
   verbose: true,
-  setupFilesAfterEnv: ['<rootDir>/.test/setup.ts']
+  setupFilesAfterEnv: ["<rootDir>/.test/setup.ts"],
 };
--- mosakin-frontend/webpack.config.base.js
+++ mosakin-frontend/webpack.config.base.js
@@ -1,5 +1,6 @@
 // モジュールの解決などはこちらに記載（storybookにも適用させるため）
 
+// eslint-disable-next-line @typescript-eslint/no-var-requires
 const path = require("path");
 const file = (...args) => path.resolve(__dirname, ...args);
 
@@ -9,26 +10,23 @@ module.exports = {
       {
         test: [/\.js$/, /\.ts$/, /\.tsx$/],
         exclude: /node_modules/,
-        loader: ["babel-loader"]
+        loader: ["babel-loader"],
       },
       {
-        test:/\.css$/,
-        use: [
-          'style-loader',
-          'css-loader'
-        ],
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
       },
       {
         test: /\.(jpe?g|png|gif|svg)$/i,
         loader: "file-loader",
       },
-    ]
+    ],
   },
   resolve: {
     extensions: [".js", ".jsx", ".ts", ".tsx"],
     modules: ["node_modules"],
     alias: {
-      "@": file("src")
-    }
-  }
+      "@": file("src"),
+    },
+  },
 };
